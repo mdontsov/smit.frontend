2b32c9376d586b6fa17dd680a9fb85a6bcf4d837
mdontsov <mdontsov@live.com>
2019-01-12 09:09:14 +0200

    ...

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TokenService } from '../authorization/token.service';
 
 @Component({
